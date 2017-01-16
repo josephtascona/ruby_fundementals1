8793b2b4631feb61cc89586a3f0e34d296433109
tasconajoey <joey.tascona@gmail.com>
2017-01-16 15:13:43 -0500

    Finish second exercise

---
+++ exercise2.rb
@@ -0,0 +1,5 @@
+# I gave 10% tip
+puts "This is a fair tip, #{55 * 0.1} dollars for 55 dollar meal"
+puts "Hello " + 1.to_s
+puts "This is a large number #{45628 * 7839}"
+(10 < 20 && 30 < 20) || !(10 == 11)
